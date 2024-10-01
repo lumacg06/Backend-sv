b9f96bf29add6b304c3997244fcbf84a28180e6d
Luisa Chavarria <luisachavarria06@gmail.com>
2024-10-01 13:46:03 -0500

    Se hicieron correciones en los models Discapacidades y Etnia

--- Desarrollosaludvida/src/main/java/com/ips/Desarrollosaludvida/models/DiscapacidadModel.java
+++ Desarrollosaludvida/src/main/java/com/ips/Desarrollosaludvida/models/DiscapacidadModel.java
@@ -1,17 +1,23 @@
 package com.ips.Desarrollosaludvida.models;
 
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "discapacidad")
 public class DiscapacidadModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- Desarrollosaludvida/src/main/java/com/ips/Desarrollosaludvida/models/EtniaModel.java
+++ Desarrollosaludvida/src/main/java/com/ips/Desarrollosaludvida/models/EtniaModel.java
@@ -4,14 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "etnia")
 public class EtniaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
